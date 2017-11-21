2cac32df3d43343749621d8471f0e1b376af328f
Unknown <milkarm27@gmail.com>
2017-11-21 17:25:53 +0700

    ทำข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Ms. : " << "Sirinthip Chuthong" << endl;
+  cout << "My nickname is : Milk " << endl;
+  cout << "Sex : Famale" << endl;
+  cout << "Birthday : 07 November 1998" << endl;
+  cout << "Hobbies : watch a movie" << endl;
+
   return 0;
 }
