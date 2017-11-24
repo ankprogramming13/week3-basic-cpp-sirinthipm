a4b76ef76f2b99c4bcaf4725bb2c3cf167e77f06
Unknown <milkarm27@gmail.com>
2017-11-24 14:13:08 +0700

    ทำข้อ3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-  
+
+  const float pi = 3.14315926536;
+  float r;
+  float h = 15;
+  cin >> r;
+
+  cout << "Area : " << pi *r* r;
+    cout << "Round : " << pi *2* r;
+    cout << "Volure :" << pi *r* r *h;
   return 0;
 }
